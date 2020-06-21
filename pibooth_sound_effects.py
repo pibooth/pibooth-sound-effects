6e78cbfb1e3a1fcda6990415f1458bfc6f1d333f
werdeil <werdeil@hotmail.com>
2020-06-21 22:44:24 +0200

    Add hook for all entering states and normalize sounds names

--- pibooth_sound_effects.py
+++ pibooth_sound_effects.py
@@ -9,7 +9,8 @@ import pygame
 import pibooth
 
 
-__version__ = "0.0.1"
+__version__ = "0.0.2"
+
 
 
 @pibooth.hookimpl
@@ -37,33 +38,74 @@ def pibooth_startup(app, cfg):
         if sound.endswith(".wav"):
             app.sounds[sound[:-4]] = pygame.mixer.Sound(os.path.join(cfg.getpath('SOUNDS', 'sounds_path'), sound))
 
-@pibooth.hookimpl
-def state_chosen_enter(app):
-    """Start a new capture sequence."""
-    app.sounds["button"].play()
-
-@pibooth.hookimpl
-def state_choose_enter(app):
-    """Start a new capture sequence."""
-    app.sounds["button"].play()
-
-@pibooth.hookimpl
-def state_capture_enter(app):
-    """Capture state"""
-    app.sounds['shutter'].play()
-
-@pibooth.hookimpl
-def state_preview_enter(app):
-    """Preview state"""
-    app.sounds['3beeps'].play()
-
-@pibooth.hookimpl
-def state_finish_enter(app):
-    """finished state"""
-    app.sounds['applause'].play()
 
 @pibooth.hookimpl
 def pibooth_cleanup():
     """Quit the mixer
     """
     pygame.mixer.quit()
+
+#--- Wait State ---------------------------------------------------------------
+@pibooth.hookimpl
+def state_wait_enter(app):
+    """Actions performed when application enter in Wait state.
+    """
+    if "wait" in app.sounds:
+        app.sounds["wait"].play()
+
+
+#--- Choose State -------------------------------------------------------------
+@pibooth.hookimpl
+def state_choose_enter(app):
+    """Actions performed when application enter in Choose state.
+    """
+    if "choose" in app.sounds:
+        app.sounds["choose"].play()
+
+#--- Chosen State -------------------------------------------------------------
+@pibooth.hookimpl
+def state_chosen_enter(app):
+    """Actions performed when application enter in Chosen state.
+    """
+    if "chosen" in app.sounds:
+        app.sounds["chosen"].play()
+
+#--- Preview State ------------------------------------------------------------
+@pibooth.hookimpl
+def state_preview_enter(app):
+    """Actions performed when application enter in Preview state.
+    """
+    if "preview" in app.sounds:
+        app.sounds["preview"].play()
+
+#--- Capture State ------------------------------------------------------------
+@pibooth.hookimpl
+def state_capture_enter(app):
+    """Actions performed when application enter in Capture state.
+    """
+    if "capture" in app.sounds:
+        app.sounds["capture"].play()
+
+#--- Processing State ---------------------------------------------------------
+@pibooth.hookimpl
+def state_processing_enter(app):
+    """Actions performed when application enter in Processing state.
+    """
+    if "processing" in app.sounds:
+        app.sounds["processing"].play()
+
+#--- PrintView State ----------------------------------------------------------
+@pibooth.hookimpl
+def state_print_enter(app):
+    """Actions performed when application enter in Print state.
+    """
+    if "print" in app.sounds:
+        app.sounds["print"].play()
+
+#--- Finish State -------------------------------------------------------------
+@pibooth.hookimpl
+def state_finish_enter(app):
+    """Actions performed when application enter in Print state.
+    """
+    if "finish" in app.sounds:
+        app.sounds["finish"].play()
